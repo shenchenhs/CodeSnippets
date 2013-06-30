ae60560c4a91c3af7116dcfac411e3bbf49eecee
shenchenhs <shenchenhs@gmail.com>
2013-06-30 21:09:00 +0800

    add dlopen()s

---
+++ dlopen/dynamic.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int f(const char* s)
+{
+        printf("hello,%s\n",s);
+        return 0;
+}
+
+int g()
+{
+        printf("what a fucking day");
+        return 0;
+}
